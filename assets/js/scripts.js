f965663a48711fcc1e73264924f792d61da13bcf
thirdsun <matthew@thirdsundev.com>
2017-04-30 11:49:52 -0700

    initial files

---
+++ assets/js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  $('#map').usmap({});
+
+  $('#map').usmap({
+    // The click action
+    click: function(event, data) {
+      $('#clicked-state')
+        .text('You clicked: '+data.name)
+        .parent().effect('highlight', {color: '#C7F464'}, 2000);
+    }
+  });
+
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    new WOW().init();
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('#collapsingNavbar li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggler:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+});
